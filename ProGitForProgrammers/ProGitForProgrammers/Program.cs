e0e6c8b921bceffa77b983a58b5b626403dab2c5
bearzone <bearzone@outlook.com>
2024-01-20 15:59:19 +0800

    marge

--- ProGitForProgrammers/ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -9,11 +9,8 @@ namespace ProGitForProgrammers
             Console.WriteLine("Hello, World!");
             Console.WriteLine("I just added this in Visual studio");
 
-<<<<<<< HEAD
             Console.WriteLine("This line add to show the staging area");
-=======
             Console.WriteLine("This line add to show the staging area - visual studio");
->>>>>>> 81dd92e16a139dafc1c82a5addf7f8e7879fd533
         }
     }
 }
