a40f5264300fc19bd14a5ba6c0662a5ea8271f17
bearzone <bearzone@outlook.com>
2024-01-20 15:29:32 +0800

    Add informative line

--- ProGitForProgrammers/ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+namespace ProGitForProgrammers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("I just added this in Visual studio");
+        }
+    }
+}
+
